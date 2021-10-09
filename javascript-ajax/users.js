07a2d8341ebd0f54b5d7ed18d27ed1b119528467
Jacob Stone <Stonedev12@gmail.com>
2021-10-09 17:44:18 +0000

    Completed javascript-ajax .

---
+++ javascript-ajax/users.js
@@ -0,0 +1,18 @@
+var userList = document.getElementById('user-list');
+console.log(userList);
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+
+xhr.addEventListener('load', function () {
+  console.log(xhr.status);
+  console.log(xhr.response);
+  for (var i = 0; i < xhr.response.length; i++) {
+    var newLi = document.createElement('li');
+    newLi.textContent = xhr.response[i].name;
+    userList.appendChild(newLi);
+  }
+});
+
+xhr.send();
