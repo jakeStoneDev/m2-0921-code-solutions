a46b41125364335cab0c55e1ffae7b3b00260956
Jacob Stone <Stonedev12@gmail.com>
2021-11-09 03:13:47 +0000

    Added new line to the write file.

--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -2,15 +2,15 @@ const fs = require('fs');
 
 var num1;
 var str;
+
 function getRandomInt(max) {
   num1 = Math.floor(Math.random() * max);
   str = num1.toString();
 }
-
 getRandomInt(100000);
 
 /* eslint-disable no-console */
-fs.writeFile('random.txt', str, 'utf8', err => {
+fs.writeFile('random.txt', str + '\n', 'utf8', err => {
   if (err) throw err;
   console.log(str);
 });
