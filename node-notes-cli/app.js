6f2c16e6e19723f6912a99dcc5c2947e18e20fe4
Jacob Stone <Stonedev12@gmail.com>
2021-11-10 03:44:44 +0000

    User can create new notes.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -11,3 +11,22 @@ if (process.argv[2].toString() === 'read') {
     }
   });
 }
+
+if (process.argv[2].toString() === 'create') {
+  fs.readFile('data.json', 'utf-8', function read(err, data) {
+    if (err) {
+      throw err;
+    }
+    const content = JSON.parse(data);
+    console.log('content:', content);
+
+    const thisKey = content.nextId;
+    content.nextId = thisKey + 1;
+    console.log(content.nextId);
+    const value = process.argv[3];
+    Object.assign(content.notes, { [thisKey]: value });
+
+    var contentStri = JSON.stringify(content);
+    fs.writeFileSync('data.json', contentStri);
+  });
+}
