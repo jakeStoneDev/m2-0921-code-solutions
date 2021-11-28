ae2488e2cdcf6a02f198bc49433f8b0b304331ee
Jacob Stone <Stonedev12@gmail.com>
2021-11-28 23:37:08 +0000

    Updated to show proper error codes.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.get('/api/notes', function (req, res) {
   const arr = [];
   fs.readFile('./data.json', function (err, data) {
-    if (err) { res.status(404).send('This note does not exist'); }
+    if (err) { res.status(500).send('Something went wrong'); }
     data = JSON.parse(data);
     for (const keys in data.notes) {
       arr.push(data.notes[keys]);
@@ -20,23 +20,31 @@ app.get('/api/notes', function (req, res) {
 
 // User can GET a single note by id
 app.get('/api/notes/:id', function (req, res) {
+  // if (!isNaN(parseInt(req.params.id))) { res.status(400).send('Bad Request'); }
   const Id = parseInt(req.params.id);
-  fs.readFile('./data.json', 'utf8', function (err, data) {
-    if (err) { res.status(404).send('This note does not exist'); }
-    data = JSON.parse(data);
-    if (data.notes[Id] === undefined) {
-      res.status(404).send('This note does not exist');
-    } else if (data.notes[Id].id === Id) {
-      res.status(200);
-      res.json(data.notes[Id]);
-    }
-  });
+  if (isNaN(Id) || Id <= 0) {
+    res.status(400).send('Bad Request');
+  } else {
+    fs.readFile('./data.json', 'utf8', function (err, data) {
+      if (err) { res.status(404).send('This note does not exist'); }
+      data = JSON.parse(data);
+      if (data.notes[Id] === undefined) {
+        res.status(404).send('This note does not exist');
+      } else if (data.notes[Id].id === Id) {
+        res.status(200);
+        res.json(data.notes[Id]);
+      }
+    });
+  }
 });
 
 // User can create a new note
 app.post('/api/notes', function (req, res) {
   fs.readFile('data.json', function (err, data) {
-    if (err) { res.status(500).send('An unexpected error occurred.'); }
+    if (err) {
+      res.status(500).send('An unexpected error occurred.');
+      res.end();
+    }
     const newObj = req.body;
     if (!newObj.content) {
       res.status(400).send('All posts must include content');
